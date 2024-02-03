95b1d52fa947e3198c61610f2c0b697f98baf821
wegar2 <awegrzyn17@gmail.com>
2024-02-03 17:22:16 +0100

    Started work on generic datasets generation methods and added levels validation to class vartype

--- src/datasets_generator.py
+++ src/datasets_generator.py
@@ -7,6 +7,18 @@ from src.var_type import VarType
 class DatasetsGenerator:
 
     @classmethod
+    def get_generic_regression_dataset(cls, only_continuous_indep: bool = True):
+        dep_vars = [Var(var_type=VarType.CONTINUOUS)] * 2
+        indep_vars = [Var(var_type=VarType.CONTINUOUS)] * 5
+        if not only_continuous_indep:
+            indep_vars += [Var(var_type=VarType.BINARY)]
+        return DatasetsGenerator.get_noise_dataset(dep_vars=dep_vars, indep_vars=indep_vars, rows_num=2_000, seed=321)
+
+    @classmethod
+    def get_generic_classification_dataset(cls, only_continuous_dep: bool = True):
+        pass
+
+    @classmethod
     def get_noise_dataset(
             cls,
             dep_vars: Var | list[Var],
--- src/var.py
+++ src/var.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, ConfigDict, PositiveInt
+from pydantic import BaseModel, ConfigDict, PositiveInt, model_validator
 from src.var_type import VarType
 
 
@@ -9,3 +9,12 @@ class Var(BaseModel):
     var_type: VarType
     num_of_levels: Optional[PositiveInt] = None
 
+    @model_validator(mode="before") # noqa
+    @classmethod
+    def _validate_discrete_var(cls, values):
+        if values["var_type"] == VarType.BINARY:
+            values["num_of_levels"] = 2
+        if values["var_type"] == VarType.MULTICLASS and values["num_of_levels"] <= 2:
+            raise ValueError("Incorrect number of levels of multiclass variable detected! "
+                             "Number of levels of multiclass variable has to be at least 3! ")
+        return values
